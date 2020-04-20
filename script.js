921138be6204083ba9c4c47885750b0df041d205
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2020-04-20 15:18:28 -0400

    added more comments and indentation

--- script.js
+++ script.js
@@ -43,45 +43,46 @@ speedyClick.startGame = function () {
             // Start countdown bar animation
             $(".countdownBar").attr("style", "display: inline-block;");
 
-        // When time runs out
-        if (timeleft <= 0) {
+            // When time runs out
+            if (timeleft <= 0) {
             
-            // Stop countdown
-            clearInterval(timer);
-            $("#countdown").html(``);
+                // Stop countdown
+                clearInterval(timer);
+                $("#countdown").html(``);
 
-            // Hide countdown bar animation
-            $(".countdownBar").attr("style", "display: none;");
+                // Hide countdown bar animation
+                $(".countdownBar").attr("style", "display: none;");
 
-            // Load Twitter button
-            $("#twitter").html(
-                `<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="I just got a new high score on Speedy Click! Think you can beat my score?" data-url="https://lucassilbernagel.github.io/lucasSilbernagelProjectThree/" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`
-            );
+                // Load Twitter button
+                $("#twitter").html(
+                    `<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="I just got a new high score on Speedy Click! Think you can beat my score?" data-url="https://lucassilbernagel.github.io/lucasSilbernagelProjectThree/" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`
+                );
 
-            // Display score
-            $("#results").html(
-                `You got ${score} clicks, an average of ${average} clicks per second!`
-            );
-            $("#results").attr("style", "background-color: #87CEFA;");
+                // Display score
+                $("#results").html(
+                    `You got ${score} clicks, an average of ${average} clicks per second!`
+                );
+                $("#results").attr("style", "background-color: #87CEFA;");
 
-            // Display Try Again button
-            $(".again").attr("style", "display: inline-block;");
+                // Display Try Again button
+                $(".again").attr("style", "display: inline-block;");
 
-            // Reset instructions
-            $("#instructions").html(``);
-            $("#instructions").attr("style", "animation: none;");
+                // Reset instructions
+                $("#instructions").html(``);
+                $("#instructions").attr("style", "animation: none;");
 
-            // Change cursor back to arrow
-            $("#clicker").attr("style", "cursor:arrow");
+                // Change cursor back to arrow
+                $("#clicker").attr("style", "cursor:arrow");
 
-            alert("Time's up!");
+                alert("Time's up!");
 
         // While countdown is still running
         } else {
-        // Display seconds remaining
-        $("#results").html(`${timeleft} seconds remaining`);
-        $("#results").attr("style", "background-color: none;");
-        }
+                
+            // Display seconds remaining
+            $("#results").html(`${timeleft} seconds remaining`);
+            $("#results").attr("style", "background-color: none;");
+            }
 
         timeleft -= 1;
         }, 1000);
